694abd14c4fc9a7ab73dd92fa3b98dac5cb3be2e
Nicolas Baudon <nicobaudon01@gmail.com>
2018-12-03 18:44:12 -0300

    Commit con componente menu

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" >
-        <Navbar light color="dark">
+      <div>
+        <Navbar dark color="dark">
           <div className="container-fluid">
-            <NavbarBrand href="/">Confusion</NavbarBrand>
+            <NavbarBrand href="/" className="titulo">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
