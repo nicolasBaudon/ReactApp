ec7b7745af170d57c1dc29ad4c9950568c87fa5d
Nicolas Baudon <nicobaudon01@gmail.com>
2018-12-07 15:40:59 -0300

    Commit con Routing incorporado, barra de navegacion hacia los componentes haciendo uso de routing, al igual que los links del footer

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import './ComponentsCss/Header.css';
+import { Navbar, NavbarBrand, Jumbotron, Nav, NavbarToggler, Collapse, NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom'; /*Para hacer los links que naveguen con el Router*/
+
 
 class Header extends Component {
 
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavOpen: false
+        }
+        this.toggleNav = this.toggleNav.bind(this); {/*Declarando esto permitimos que el metodo toggleNav pueda ser llamado usando this.toggleNav*/ }
+    }
 
+    toggleNav() {
+        this.setState({ isNavOpen: !this.state.isNavOpen })
+    }
+
+    render() {
         return (
             <> {/*Esto se llama React Fragment, retorna todos los elementos que queramos sin que el padre sea un div. Esto es porque el return tiene que devolver un solo elemento*/}
-                <Navbar dark color="dark">
+                <Navbar dark color="dark" expand="md"> {/* IMPORTANTE: El expand indica, en reactstrap, que la barra se va a colapsar para dispositivos mas chicos que los medianos, osea para los celulares y tablets se colapsa*/}
                     <div className="container-fluid">
-                        <NavbarBrand href="/" className="titulo">Ristorante Con Fusion</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav} /> {/*El onClick llama a esa funcion que cambia el state a true y muestra la navegacion*/}
+                        <NavbarBrand className="mr-auto" href="/" className="titulo"><img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" /></NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar> {/*El isNavOpen funciona para que la barra se colapse, si es false no se muestra, si es true si*/}
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home"><span className="fa fa-home fa-lg"></span> Home</NavLink> {/*La className nav-link, pone los botones bonitos para que se vean bien en la navegacion*/}
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus"><span className="fa fa-info fa-lg"></span>   About</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/menu"><span className="fa fa-list fa-lg"></span> Menu</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus"><span className="fa fa-address-card fa-lg"></span> Contact</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
                 <Jumbotron>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../compartida/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -11,21 +13,34 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
-            selectedDish: null
+            /*selectedDish: null -- Esto lo usaba para seleccionar el plato y mandarlo al DishDetail*/
         };
     }
 
-    cambiarEstado(dishId) {
-        this.setState({ selectedDish: dishId });
-    }
+    /*cambiarEstado(dishId) {
+                this.setState({ selectedDish: dishId }); ------ Esto cabiaba el selectedDish para mandar ese plato desp al DishDetail
+            }*/
 
     render() {
+
+        const HomePage = () => {
+            return (
+                <Home /> /*Esto lo hace asi para despues pasarle atributos al Home*/
+            );
+        }
+
+
         return (
             <div>
                 <Header />
-                <Menu dishes={this.state.dishes}
-                    onClick={(dishId) => this.cambiarEstado(dishId)} />
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> {/* le pone el cero para seleccionar el primer objeto del arreglo que devuelve la funcion filter, por mas que sea 1 objeto */}
+                {/*<Menu dishes={this.state.dishes}
+                            onClick={(dishId) => this.cambiarEstado(dishId)} /> -- De esta forma llama al Menu sin el routing*/}
+                {/*<DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />  le pone el cero para seleccionar el primer objeto del arreglo que devuelve la funcion filter, por mas que sea 1 objeto */}{/*De esta forma llamaba al DishDetail sin el routing*/}
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Redirect to="/home" />
+                </Switch>
                 <Footer />
             </div>
         );
