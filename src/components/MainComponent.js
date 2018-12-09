dd10e2dce5eb3744c4d44499942d4f8454c5aad5
Nicolas Baudon <nicobaudon01@gmail.com>
2018-12-08 22:16:37 -0300

    Commit con Routing incorporado, barra de navegacion hacia los componentes haciendo uso de routing, al igual que los links del footer. Componente Home desarrollado usando los parametro pasado desde el main, consumiendo desde los jsons

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,7 +4,11 @@ import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../compartida/dishes';
+import { COMMENTS } from '../compartida/comments';
+import { LEADERS } from '../compartida/leader';
+import { PROMOTIONS } from '../compartida/promotion';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -13,6 +17,9 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS
             /*selectedDish: null -- Esto lo usaba para seleccionar el plato y mandarlo al DishDetail*/
         };
     }
@@ -23,9 +30,11 @@ class Main extends Component {
 
     render() {
 
-        const HomePage = () => {
+        const HomePage = () => { /*Esto lo hace asi para despues pasarle atributos al Home*/
             return (
-                <Home /> /*Esto lo hace asi para despues pasarle atributos al Home*/
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((lead) => lead.featured)[0]} />
             );
         }
 
@@ -38,7 +47,8 @@ class Main extends Component {
                 {/*<DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />  le pone el cero para seleccionar el primer objeto del arreglo que devuelve la funcion filter, por mas que sea 1 objeto */}{/*De esta forma llamaba al DishDetail sin el routing*/}
                 <Switch>
                     <Route path="/home" component={HomePage} />
-                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} /> {/*Otra forma de pasar atributos a los componentes usando Routing*/}
+                    <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
