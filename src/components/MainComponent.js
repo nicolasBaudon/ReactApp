61b9f111c26e75974c3bb430c8da8a5af878befc
Nicolas Baudon <nicobaudon01@gmail.com>
2018-12-14 16:26:28 -0300

    Commit con Redux implementado. Carpeta redux para contener el estado y crear el store. Video - Introduction to Redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,23 +6,22 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
-import { DISHES } from '../compartida/dishes';
-import { COMMENTS } from '../compartida/comments';
-import { LEADERS } from '../compartida/leader';
-import { PROMOTIONS } from '../compartida/promotion';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        promotions: state.promotions,
+        leaders: state.leaders
+    }
+}
 
 class Main extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            dishes: DISHES,
-            comments: COMMENTS,
-            leaders: LEADERS,
-            promotions: PROMOTIONS
-            /*selectedDish: null -- Esto lo usaba para seleccionar el plato y mandarlo al DishDetail*/
-        };
     }
 
     /*cambiarEstado(dishId) {
@@ -33,37 +32,36 @@ class Main extends Component {
 
         const HomePage = () => { /*Esto lo hace asi para despues pasarle atributos al Home*/
             return (
-                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-                    leader={this.state.leaders.filter((lead) => lead.featured)[0]} />
+                <Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.props.leaders.filter((lead) => lead.featured)[0]} />
             );
         };
 
         const DishWithId = ({ match }) => {     /*Tambien le llega location y history pero solo usamos match*/
             return (
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
-                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+                <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
             );
         };
 
         return (
             <div>
                 <Header />
-                {/*<Menu dishes={this.state.dishes}
-                            onClick={(dishId) => this.cambiarEstado(dishId)} /> -- De esta forma llama al Menu sin el routing*/}
-                {/*<DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />  le pone el cero para seleccionar el primer objeto del arreglo que devuelve la funcion filter, por mas que sea 1 objeto */}{/*De esta forma llamaba al DishDetail sin el routing*/}
-                <Switch>
-                    <Route path="/home" component={HomePage} />
-                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} /> {/*Otra forma de pasar atributos a los componentes usando Routing*/}
-                    <Route path="/menu/:dishId" component={DishWithId} />
-                    <Route exact path="/contactus" component={Contact} />
-                    <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
-                    <Redirect to="/home" />
-                </Switch>
+                <div>
+                    <Switch>
+                        <Route path='/home' component={HomePage} />
+                        <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
+                        <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
+                        <Route path='/menu/:dishId' component={DishWithId} />
+                        <Route exact path='/contactus' component={Contact} />
+                        <Redirect to="/home" />
+                    </Switch>
+                </div>
                 <Footer />
             </div>
         );
     }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main)); /*Aca lo que hace es conectar la aplicacion con el store, con el connect y que ande el routing, con el withRouter*/
